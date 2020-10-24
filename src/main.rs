9997ac4fcd9387907c4686a956233df8e536a9d3
Stephen Leyva <sleyva1297@gmail.com>
2020-10-24 15:09:24 -0700

    Unittests

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 extern crate rlibc;
+mod serial;
 mod vga_buffer;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
+    Success = 0x10, // 16 (00010000 << 1 | 1) == (00100000 | 00000001) == 00100001 == 33
+    Failed = 0x11,  // 17 (00010001 << 1 | 1) == (00100010 | 00000001) == 00100011 == 35
 }
 
 pub fn exit_qemu(exit_code: QemuExitCode) {
@@ -24,22 +26,56 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World: {}", "sleyva_os");
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World: {}", "sleyva_os");
+
+    #[cfg(test)]
+    test_main();
+    loop {}
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
     }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trival_assertion() {
+    assert_eq!(1, 1);
 }
