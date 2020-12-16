cecab6a06d4ea73b8132b0b88f216f1df738ef2c
Stephen Leyva <sleyva1297@gmail.com>
2020-12-15 22:48:40 -0800

    add PCI interrupt handlers

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,33 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(sleyva_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-extern crate rlibc;
-mod serial;
-mod vga_buffer;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10, // 16 (00010000 << 1 | 1) == (00100000 | 00000001) == 00100001 == 33
-    Failed = 0x11,  // 17 (00010001 << 1 | 1) == (00100010 | 00000001) == 00100011 == 35
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
+use sleyva_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World: {}", "sleyva_os");
 
+    sleyva_os::init();
+
     #[cfg(test)]
     test_main();
-    loop {}
+    sleyva_os::hlt_loop();
 }
 
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    sleyva_os::hlt_loop();
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trival_assertion() {
-    assert_eq!(1, 1);
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    sleyva_os::test_panic_handler(info)
 }
